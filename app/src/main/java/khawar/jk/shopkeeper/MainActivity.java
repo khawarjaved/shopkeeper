fd5a1dcd37fbabae8cff9ca67a73b7b76575bf88
khawa <khavarjk@gmail.com>
2020-05-09 19:34:36 +0500

    add button and setOnclicklistener

--- app/src/main/java/khawar/jk/shopkeeper/MainActivity.java
+++ app/src/main/java/khawar/jk/shopkeeper/MainActivity.java
@@ -3,6 +3,7 @@ package khawar.jk.shopkeeper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -26,5 +27,13 @@ Button buttonlogin;
 
         ArrayAdapter <CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.usertype,R.layout.support_simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+        buttonlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String item = spinner.getSelectedItem().toString();
+                if (username.getText().toString().equals("admin")&& userpassword.getText().toString().equals("admin")&& item.equals("admin"));
+                
+            }
+        });
     }
 }
