7499ba9a4093ccbe564eb45e19eeeffccaa891cd
khawa <khavarjk@gmail.com>
2020-05-09 18:27:00 +0500

    add EidtText,spinner,button array adapter add decler resource path and spinner

--- app/src/main/java/khawar/jk/shopkeeper/MainActivity.java
+++ app/src/main/java/khawar/jk/shopkeeper/MainActivity.java
@@ -3,12 +3,28 @@ package khawar.jk.shopkeeper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
-
+EditText username;
+EditText userpassword;
+Spinner spinner;
+Button buttonlogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+      username  = (EditText) findViewById(R.id.username);
+      userpassword = (EditText) findViewById(R.id.userpassword);
+      spinner = (Spinner) findViewById(R.id.spinner);
+      buttonlogin = (Button) findViewById(R.id.buttonlogin);
+
+
+        ArrayAdapter <CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.usertype,R.layout.support_simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 }
