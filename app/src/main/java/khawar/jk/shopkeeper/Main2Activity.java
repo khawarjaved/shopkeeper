e2dc15db9a1647fe90dd2d97aaa1730b75854dfa
khawa <khavarjk@gmail.com>
2020-05-11 02:03:53 +0500

    add imageview , textview, id

--- app/src/main/java/khawar/jk/shopkeeper/Main2Activity.java
+++ app/src/main/java/khawar/jk/shopkeeper/Main2Activity.java
@@ -4,20 +4,29 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
+    ImageView imageView;
+    TextView title, description;
+    int position;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         ActionBar actionBar = getSupportActionBar();
-        
+
         if(actionBar !=null){
             actionBar.setDisplayHomeAsUpEnabled(true);
             actionBar.setDisplayShowHomeEnabled(true);
 
         }
+        imageView = findViewById(R.id.imagenext);
+        title = findViewById(R.id.titleText);
+        description = findViewById(R.id.descriptionText);
+
     }
 
 }
