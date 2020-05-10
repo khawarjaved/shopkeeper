f728e038543db45d123dbdf5d7cd47b98f1e94fd
khawa <khavarjk@gmail.com>
2020-05-11 02:00:43 +0500

    add action bar display set to condition display show or off

--- app/src/main/java/khawar/jk/shopkeeper/Main2Activity.java
+++ app/src/main/java/khawar/jk/shopkeeper/Main2Activity.java
@@ -1,5 +1,6 @@
 package khawar.jk.shopkeeper;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,5 +11,13 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        ActionBar actionBar = getSupportActionBar();
+        
+        if(actionBar !=null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setDisplayShowHomeEnabled(true);
+
+        }
     }
+
 }
