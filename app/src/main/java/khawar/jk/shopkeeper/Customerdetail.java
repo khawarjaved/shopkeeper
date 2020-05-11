1162415578e793885bb09c77a4385d56bd98908d
khawa <khavarjk@gmail.com>
2020-05-12 01:58:56 +0500

    using addData function in insert data and call toast method

--- app/src/main/java/khawar/jk/shopkeeper/Customerdetail.java
+++ app/src/main/java/khawar/jk/shopkeeper/Customerdetail.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class Customerdetail extends AppCompatActivity {
 Button addbutton,viewbutton;
@@ -14,5 +15,22 @@ DatabaseHalper mDatabasehalper;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customerdetail);
+        addbutton = findViewById(R.id.addbutton);
+        viewbutton = findViewById(R.id.viewbutton);
+        editcustomername= findViewById(R.id.editcustomername);
+
+        mDatabasehalper = new DatabaseHalper(this);
+    }
+    public void AddData(String newEntry){
+        boolean insertData = mDatabasehalper.addData(newEntry);
+        if (insertData){
+            toastMessage("Data successfully inserted");
+        }else {
+            toastMessage("Something went wrong");
+        }
+    }
+
+    private void toastMessage(String message) {
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
     }
 }
