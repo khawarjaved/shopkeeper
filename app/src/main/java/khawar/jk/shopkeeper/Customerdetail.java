9e7150282999066e32948b3e83a5ec3af6c7ccc9
khawa <khavarjk@gmail.com>
2020-05-12 00:46:01 +0500

    add button main2activity to move next activity

---
+++ app/src/main/java/khawar/jk/shopkeeper/Customerdetail.java
@@ -0,0 +1,14 @@
+package khawar.jk.shopkeeper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Customerdetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customerdetail);
+    }
+}
--- app/src/main/java/khawar/jk/shopkeeper/Main2Activity.java
+++ app/src/main/java/khawar/jk/shopkeeper/Main2Activity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,7 +14,7 @@ public class Main2Activity extends AppCompatActivity {
     ImageView imageView;
     TextView title, description;
     int position;
-
+     Button Customerdetail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +29,19 @@ public class Main2Activity extends AppCompatActivity {
         imageView = findViewById(R.id.imagenext);
         title = findViewById(R.id.titleText);
         description = findViewById(R.id.descriptionText);
+        Customerdetail = findViewById(R.id.customerdetail);
+        Customerdetail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivityCustomer();
+
+            }
+
+            public void openActivityCustomer() {
+                Intent intent = new Intent(Main2Activity.this,Customerdetail.class);
+                startActivity(intent);
+            }
+        });
 
         if (position==0){
             Intent intent = getIntent();
@@ -41,7 +56,136 @@ public class Main2Activity extends AppCompatActivity {
 
             actionBar.setTitle(aTitle);
         }
+        if (position==1){
+            Intent intent = getIntent();
+            Bundle bundle = this.getIntent().getExtras();
+            int pic = bundle.getInt("image");
+            String aTitle = intent.getStringExtra("title");
+            String aDescription = intent.getStringExtra("description");
 
+            imageView.setImageResource(pic);
+            title.setText(aTitle);
+            description.setText(aDescription);
+
+            actionBar.setTitle(aTitle);
+        }
+        if (position==2){
+            Intent intent = getIntent();
+            Bundle bundle = this.getIntent().getExtras();
+            int pic = bundle.getInt("image");
+            String aTitle = intent.getStringExtra("title");
+            String aDescription = intent.getStringExtra("description");
+
+            imageView.setImageResource(pic);
+            title.setText(aTitle);
+            description.setText(aDescription);
+
+            actionBar.setTitle(aTitle);
+        }
+        if (position==3){
+            Intent intent = getIntent();
+            Bundle bundle = this.getIntent().getExtras();
+            int pic = bundle.getInt("image");
+            String aTitle = intent.getStringExtra("title");
+            String aDescription = intent.getStringExtra("description");
+
+            imageView.setImageResource(pic);
+            title.setText(aTitle);
+            description.setText(aDescription);
+
+            actionBar.setTitle(aTitle);
+        }
+        if (position==4){
+            Intent intent = getIntent();
+            Bundle bundle = this.getIntent().getExtras();
+            int pic = bundle.getInt("image");
+            String aTitle = intent.getStringExtra("title");
+            String aDescription = intent.getStringExtra("description");
+
+            imageView.setImageResource(pic);
+            title.setText(aTitle);
+            description.setText(aDescription);
+
+            actionBar.setTitle(aTitle);
+        }
+        if (position==5){
+            Intent intent = getIntent();
+            Bundle bundle = this.getIntent().getExtras();
+            int pic = bundle.getInt("image");
+            String aTitle = intent.getStringExtra("title");
+            String aDescription = intent.getStringExtra("description");
+
+            imageView.setImageResource(pic);
+            title.setText(aTitle);
+            description.setText(aDescription);
+
+            actionBar.setTitle(aTitle);
+        }
+        if (position==6){
+            Intent intent = getIntent();
+            Bundle bundle = this.getIntent().getExtras();
+            int pic = bundle.getInt("image");
+            String aTitle = intent.getStringExtra("title");
+            String aDescription = intent.getStringExtra("description");
+
+            imageView.setImageResource(pic);
+            title.setText(aTitle);
+            description.setText(aDescription);
+
+            actionBar.setTitle(aTitle);
+        }
+        if (position==7){
+            Intent intent = getIntent();
+            Bundle bundle = this.getIntent().getExtras();
+            int pic = bundle.getInt("image");
+            String aTitle = intent.getStringExtra("title");
+            String aDescription = intent.getStringExtra("description");
+
+            imageView.setImageResource(pic);
+            title.setText(aTitle);
+            description.setText(aDescription);
+
+            actionBar.setTitle(aTitle);
+        }
+        if (position==8){
+            Intent intent = getIntent();
+            Bundle bundle = this.getIntent().getExtras();
+            int pic = bundle.getInt("image");
+            String aTitle = intent.getStringExtra("title");
+            String aDescription = intent.getStringExtra("description");
+
+            imageView.setImageResource(pic);
+            title.setText(aTitle);
+            description.setText(aDescription);
+
+            actionBar.setTitle(aTitle);
+        }
+        if (position==9){
+            Intent intent = getIntent();
+            Bundle bundle = this.getIntent().getExtras();
+            int pic = bundle.getInt("image");
+            String aTitle = intent.getStringExtra("title");
+            String aDescription = intent.getStringExtra("description");
+
+            imageView.setImageResource(pic);
+            title.setText(aTitle);
+            description.setText(aDescription);
+
+            actionBar.setTitle(aTitle);
+        }
+        if (position==10){
+            Intent intent = getIntent();
+            Bundle bundle = this.getIntent().getExtras();
+            int pic = bundle.getInt("image");
+            String aTitle = intent.getStringExtra("title");
+            String aDescription = intent.getStringExtra("description");
+
+            imageView.setImageResource(pic);
+            title.setText(aTitle);
+            description.setText(aDescription);
+
+            actionBar.setTitle(aTitle);
+        }
     }
 
 }
