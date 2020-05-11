8d3a2564015f4253de43fb31fdb2ed624beb2cfe
khawa <khavarjk@gmail.com>
2020-05-12 01:36:02 +0500

    databasehalper class add

--- app/src/main/java/khawar/jk/shopkeeper/Customerdetail.java
+++ app/src/main/java/khawar/jk/shopkeeper/Customerdetail.java
@@ -3,9 +3,13 @@ package khawar.jk.shopkeeper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Customerdetail extends AppCompatActivity {
-
+Button addbutton,viewbutton;
+EditText editcustomername;
+DatabaseHalper mDatabasehalper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
---
+++ app/src/main/java/khawar/jk/shopkeeper/DatabaseHalper.java
@@ -0,0 +1,47 @@
+package khawar.jk.shopkeeper;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHalper extends SQLiteOpenHelper {
+    private static final String TAG ="DatabaseHelper";
+    private static  final String TABLE_NAME ="customer_table";
+    private static final String COL1= "ID";
+    private static final String COL2= "name";
+
+    public DatabaseHalper(@Nullable Context context) {
+        super(context,TABLE_NAME,null,1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTable = " CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT," + COL2 +"TEXT)";
+        db.execSQL(createTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL (" DROP IF TABLE EXISTS " + TABLE_NAME);
+        onCreate(db);
+
+    }
+    public boolean addData(String item){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL2,item);
+
+        Log.d(TAG," addData: Adding " + item +" to " + TABLE_NAME);
+        long result = db.insert(TABLE_NAME, null, contentValues);
+
+        if (result == -1){
+            return false;
+        }else {
+            return true;
+        }
+    }
+}
